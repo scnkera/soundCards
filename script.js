9c15ef4365bfceb51ca42589d1c3fb387f883f68
scnkera <149810869+scnkera@users.noreply.github.com>
2024-06-18 20:39:44 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('click', (event) => {
+    const card = document.getElementsByClassName('soundCard');
+    const catButton = document.getElementById("cat-button");
+    
+    card.onclick = ()=>{
+
+    }
+    // console.log('clicked');
+    // if (element) {
+    //     element.addEventListener('click', () => {
+    //         console.log('clicked');
+    //     });
+    // } else {
+    //     console.log('Element with the specified ID not found');
+    // }
+});
+
+// step 1: get all class = soundCloud
+// step 2: loop through classes
+// step 3: add onClick to each
+// step 4: figure out what the id
+// step 5: if the card id equals = x
+// step 6: play associated sound
