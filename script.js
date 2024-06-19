5c7ad99f5e0a78a96aebf2031708acc281413087
scnkera <149810869+scnkera@users.noreply.github.com>
2024-06-19 10:15:27 -0400

    Updated html, css, and javascript

--- script.js
+++ script.js
@@ -1,19 +1,76 @@
-document.addEventListener('click', (event) => {
-    const card = document.getElementsByClassName('soundCard');
-    const catButton = document.getElementById("cat-button");
-    
-    card.onclick = ()=>{
+// step 1: get all class = soundCloud
+const allCards = document.getElementsByClassName('soundCard');
 
+// card types
+
+const catCard = document.getElementByiD('cat-button');
+const dogCard = document.getElementByiD('dog-button');
+const lionCard = document.getElementByiD('lion-button');
+const lightningCard = document.getElementByiD('lightning-button');
+const rainCard = document.getElementByiD('rain-button');
+const tornadoCard = document.getElementByiD('tornado-button');
+
+// const elementId = document.getElementByClassName('.exampleClass');
+// console.log(allCards[1]);
+// console.log(allCards);
+
+// step 2: loop through classes
+for (let card of allCards) {
+    // step 3: add onClick to each
+    card.addEventListener('click', () => {
+        if 
+
+
+//     }
+    // card.onclick = (imp) => {
+    //     // const imp = allCards.outterHTML
+    //     console.log('imp')
+    //     // associatedId = allCards.id;
+    //     // console.log("clicked");
+    //     // // const cardIdName = document.getAttribute('lightning-button');
+        
+    //     // const associatedId = card.id;
+    //     // console.log(associatedId);
     }
-    // console.log('clicked');
-    // if (element) {
-    //     element.addEventListener('click', () => {
-    //         console.log('clicked');
-    //     });
-    // } else {
-    //     console.log('Element with the specified ID not found');
-    // }
-});
+}
+
+
+
+// document.addEventListener('click', (event) => {
+//     const card = document.getElementsByClassName('soundCard');
+//     const catButton = document.getElementById("cat-button");
+    
+//     card.onclick = ()=>{
+
+//     }
+//     // console.log('clicked');
+//     // if (element) {
+//     //     element.addEventListener('click', () => {
+//     //         console.log('clicked');
+//     //     });
+//     // } else {
+//     //     console.log('Element with the specified ID not found');
+//     // }
+// });
+
+
+
+// document.getElementsByClass((event) => {
+//     const card = document.getElementsByClassName('soundCard');
+//     const catButton = document.getElementById("cat-button");
+    
+//     card.onclick = ()=>{
+
+//     }
+//     // console.log('clicked');
+//     // if (element) {
+//     //     element.addEventListener('click', () => {
+//     //         console.log('clicked');
+//     //     });
+//     // } else {
+//     //     console.log('Element with the specified ID not found');
+//     // }
+// });
 
 // step 1: get all class = soundCloud
 // step 2: loop through classes
